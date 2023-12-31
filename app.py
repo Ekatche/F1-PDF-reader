bd28c1bbf921e2a4410b58043084660c8b1b7723
Ekatche <71089627+Ekatche@users.noreply.github.com>
2023-12-31 10:37:37 +0100

    restructure app

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+# This page works like a main entry page of the streamlit app
+st.title('AI Tool to extract information from PDF file')
---
+++ pages/pdf_reader.py
@@ -0,0 +1,99 @@
+import streamlit as st
+import pandas as pd 
+import numpy as np 
+import sys
+import os 
+from pathlib import Path
+st.set_page_config(page_title="PDF Reader")
+
+
+script_dir = Path(os.path.dirname(os.path.abspath(__file__))).parent
+relative_path = os.path.join(script_dir, 'functions')
+sys.path.insert(1, str(relative_path))
+
+from AppFunctions import load_model, load_data, Vus_df, format_Vus
+
+######################
+####              ####
+####  Application ####
+####              ####
+######################
+mut_url = "https://mutalyzer.nl/api/"
+
+
+# page header 
+st.set_page_config(page_title="Foundation Medecine", layout="centered")
+
+st.markdown("<h1 style='text-align:center'> Fondation Medecine page </h1>",  unsafe_allow_html=True)
+st.write(
+    """
+    This page is designed to extract VUS data from Foundation Medecine Report.
+
+    """
+    
+)
+st.info(
+    """
+    Process: 
+    1. Upload the pdf report  
+    2. Launch the preprocessing step 
+    3. Then check if the data is correct
+    4. Finaly download the data
+    """,
+    icon ="⚙️"
+)
+st.divider()
+
+classification_model, model, nlp = load_model()
+
+if 'button_1' not in st.session_state:
+    st.session_state.button_1 = False
+if 'button_2' not in st.session_state:
+    st.session_state.button_2 = False
+if 'button_3' not in st.session_state:
+    st.session_state.button_3 = False
+
+
+if 'data_loaded' not in st.session_state:
+    st.session_state.data_loaded = False
+
+def cb1():
+    st.session_state.button_1 = True
+def cb2():
+    st.session_state.button_2 = True
+def cb3():
+    st.session_state.button_3 = True
+
+uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
+button1 = st.button("Confirm",  on_click=cb1)
+st.divider()
+
+if st.session_state.button_1 :
+    try : 
+        vus_data = load_data(uploaded_file.read(), classification_model,model,nlp)           
+        if "vus_df" not in st.session_state:
+            st.session_state.vus_df = Vus_df(vus_data)
+    except Exception as e  : 
+        st.session_state.button_1 =False
+        print(e)
+else:
+    st.session_state.button_7 =False
+
+
+st.markdown("<h4 style='text-align: center; color: black; text-decoration: underline;'> Variants of Unknown Significance (VUS) </h4>", unsafe_allow_html=True) 
+if "vus_df"  in st.session_state : 
+    try : 
+        #########
+        # VUS
+        #########
+        st.info("""check data and correct errors """, icon="ℹ️")
+        Vus =  Vus_df(vus_data)
+        Vus_df = st.data_editor(Vus, num_rows="dynamic", use_container_width=True, key="UniqueVus")               
+        button = st.button("Reformat the Vus", on_click=cb8) 
+    except Exception as e  :
+        del st.session_state['vus_df']
+        print("VUS:", e)
+
+else :
+    st.info("""please load a file""", icon="ℹ️")
+
